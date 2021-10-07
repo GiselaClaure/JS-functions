8fe585f565fdf67bebbb21497431df12b6ea6281
unknown <giselaclaure@gmail.com>
2021-10-07 22:28:30 +0200

    iteracion_6

---
+++ iteracion_6.js
@@ -0,0 +1,25 @@
+// **Iteración #6: Valores únicos**
+
+const duplicates = [
+    'sushi',
+    'pizza',
+    'burger',
+    'potatoe',
+    'pasta',
+    'ice-cream',
+    'pizza',
+    'chicken',
+    'onion rings',
+    'pasta',
+    'soda'
+  ];
+  function removeDuplicates(param) {
+    let uniqueElements= []
+    for (let i = 0; i < param.length; i++) {
+        if (!uniqueElements.includes (param[i])) {
+            uniqueElements.push (param[i])
+        }
+    }
+    return console.log(uniqueElements)
+  }
+  removeDuplicates(duplicates)
